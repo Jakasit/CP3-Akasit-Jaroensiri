6e319b75050386533d25fd2c2c7a1b54897c9834
Jakasit <jakasit@gmail.com>
2020-04-28 10:19:10 +0700

    Create Exercise4_Akasit_J.py

---
+++ Assignments/Exercise4_Akasit_J.py
@@ -0,0 +1,9 @@
+FE = 10.0
+GB = 13.5
+ICS = 7.2
+CP = 15.0
+print("              YOUR SCORE           ")
+print("Foundation English              ",FE)
+print("General Business                ",GB)
+print("Introduction to Computer Systems",ICS)
+print("Computer Programming            ",CP)
